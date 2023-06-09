237b5340c7b2635d610a3f6783b6d53598d83f1f
Eyal Cohen <eyalcohen4.ec@gmail.com>
2023-06-09 13:58:12 +0300

    Mobile view

--- components/layouts/app-layout.tsx
+++ components/layouts/app-layout.tsx
@@ -1,25 +1,22 @@
 import { ReactNode } from "react"
-import { useSession } from "next-auth/react"
 
 import { AppCommand } from "../app-command"
 import { Nav } from "../nav"
 import { ProfilePicture } from "../profile-picture"
 import { ThemeToggle } from "../theme-toggle"
-import { Avatar, AvatarImage } from "../ui/avatar"
-import { Command } from "../ui/command"
 
 export const AppLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="h-screen">
       <Header />
-      <div className="grid grid-cols-[200px_1fr] h-[calc(100vh-81px)]">
-        <aside className="h-full border-r">
+      <div className="md:grid md:grid-cols-[200px_1fr] h-[calc(100vh-81px)] w-full">
+        <aside className="h-full border-r md:block hidden">
           <div className="flex flex-col items-center gap-4 mt-8">
             <Nav />
           </div>
         </aside>
-        <div className="h-full">
-          <section className="h-full p-10">{children}</section>
+        <div className="h-full w-full">
+          <section className="h-full w-full p-2 md:p-10">{children}</section>
         </div>
       </div>
     </div>
