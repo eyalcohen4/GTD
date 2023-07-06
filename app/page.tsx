b4fcc707dc68176f37ccf4fbde80d06689f49724
Eyal Cohen <eyalcohen4.ec@gmail.com>
2023-07-06 12:30:42 +0300

    home

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import dayjs from "dayjs"
 
 import { Goals } from "@/components/goals"
 import { Greeting } from "@/components/greeting"
+import { HomeTasks } from "@/components/home-tasks"
 import { AppLayout } from "@/components/layouts/app-layout"
 import { Projects } from "@/components/projects"
 import { TasksList } from "@/components/tasks-list"
@@ -17,21 +18,7 @@ export default async function IndexPage() {
         </div>
 
         <Projects />
-        <TasksList
-          title="Overdue"
-          timeRange={{
-            to: dayjs().endOf("day").subtract(1, "day").toISOString(),
-          }}
-        />
-        <TasksList
-          statuses={statuses
-            .filter(({ value }) => value !== "COMPLETED")
-            .map(({ value }) => value)}
-          title="Today"
-          timeRange={{
-            to: dayjs().endOf("day").toISOString(),
-          }}
-        />
+        <HomeTasks />
         <TasksList statuses={["INBOX"]} title="Inbox" />
       </div>
     </AppLayout>
---
+++ components/home-tasks.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { statuses } from "@/constants/statuses"
+import dayjs from "dayjs"
+
+import { TasksList } from "@/components/tasks-list"
+
+export const HomeTasks = () => {
+  return (
+    <>
+      <TasksList
+        title="Overdue"
+        timeRange={{
+          to: dayjs().endOf("day").subtract(1, "day").toISOString(),
+        }}
+      />
+      <TasksList
+        statuses={statuses
+          .filter(({ value }) => value !== "COMPLETED")
+          .map(({ value }) => value)}
+        title="Today"
+        timeRange={{
+          to: dayjs().endOf("day").toISOString(),
+        }}
+      />
+    </>
+  )
+}
